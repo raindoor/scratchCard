013172b42b7e25ad49ac8710b7d33a5275614cc2
Tommy <tommy@brauther.com>
2016-10-28 17:39:39 +0900

    이식작업

--- web/index.php
+++ web/index.php
@@ -1,25 +1,29 @@
-<?php
-
-require('../vendor/autoload.php');
-
-$app = new Silex\Application();
-$app['debug'] = true;
-
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
-
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
-
-// Our web handlers
-
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
-});
-
-$app->run();
+<!Doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>
+      Scratch Card!
+    </title>
+    <link rel="stylesheet" href="./css/scratchCard.css"></link>
+    <script src="https://code.jquery.com/jquery-1.11.3.min.js" type="text/javascript"></script>
+    <script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js" type="text/javascript"></script>
+    <script src="./js/scratchCard.js" type="text/javascript"></script>
+  </head>
+  <body>
+    <div id="panel">
+      <div id="centerCardArea"></div>
+      <div id="cardList"></div>
+      <div class="cardClickArea topCard"></div>
+      <div class="cardClickArea leftCard"></div>
+      <div class="cardClickArea rightCard"></div>
+      <div class="cardClickArea bottomCard"></div>
+    </div>
+    <div id="errorArea">Miss : <span id="errorCount">0</span></div>
+    <div id="stackArea">Stack : <span id="stackCount">0</span></div>
+    <!-- <h1 id="timer"><time>00:00:00</time></h1>
+    <div class="timerController" id="startTimer">start</div>
+    <div class="timerController" id="stopTimer">stop</div>
+    <div class="timerController" id="clearTimer">clear</div> -->
+  </body>
+</html>
